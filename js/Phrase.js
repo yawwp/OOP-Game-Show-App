122936ba15624e6ba6103918791eb105a5989752
Eric Agbayani <eric.agbayani@yahoo.com>
2022-09-11 15:31:05 -0700

    update

---
+++ js/Phrase.js
@@ -0,0 +1,24 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Phrase.js */
+
+// const phrases = [
+//     {quote: 'Never underestimate the big importance of small things'},
+//     {quote:'Do not fear failure but rather fear not trying'},
+//     {quote:'It does not matter how slowly you go as long as you do not stop'},
+//     {quote:'An ounce of action is worth a ton of theory'},
+//     {quote: 'It does not do to dwell on dreams and forget to live'}
+// ];
+
+
+
+class Phrase {
+    constructor (phrase){
+        this.phrase = [    
+        {phrases: 'Never underestimate the big importance of small things'},
+        {phrases:'Do not fear failure but rather fear not trying'},
+        {phrases:'It does not matter how slowly you go as long as you do not stop'},
+        {phrases:'An ounce of action is worth a ton of theory'},
+        {phrases: 'It does not do to dwell on dreams and forget to live'}];
+    }
+}

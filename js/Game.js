be60289bb1aa3c949a17fec413ea51d50bf1c389
Eric Agbayani <eric.agbayani@yahoo.com>
2022-09-11 18:37:19 -0700

    update

--- js/Game.js
+++ js/Game.js
@@ -3,5 +3,26 @@
  * Game.js */
 
 class Game {
+    constructor(){
+        this.missed = 0;
+        this.activePhrase = null;
+        this.phrases = 
+            [    
+                {phrase:'Never underestimate the big importance of small things'},
+                {phrase:'Do not fear failure but rather fear not trying'},
+                {phrase:'It does not matter how slowly you go as long as you do not stop'},
+                {phrase:'An ounce of action is worth a ton of theory'},
+                {phrase:'It does not do to dwell on dreams and forget to live'}
+            ];
+    }
+    
+    /**
+     * Selects random phrase from phrases property
+     * @return {object} Phrase object chosen to be used 
+     * 
+     */
+    getRandomPhrase(phrase){
+
+    };
 
 }

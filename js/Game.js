4732db371da93d2e3ce98fc971ce1f0b67e974a6
Eric Agbayani <eric.agbayani@yahoo.com>
2022-10-09 20:32:16 -0700

    update

--- js/Game.js
+++ js/Game.js
@@ -1,24 +1,22 @@
 /* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
-
+* Project 4 - OOP Game App
+* Game.js */
 class Game {
     constructor(){
         this.missed = 0;
         this.activePhrase = null;
         this.phrases = [
-            {phrase:'say hi'},
             {phrase:'gotcha'},
+            {phrase:'say hi'},
             {phrase:'hello there'},
             {phrase:'puppy junior'},
             {phrase:'fish tacos'}
         ];
     }
-    
     /**
      * Selects random phrase from phrases property
-     * @return {object} Phrase object chosen to be used 
-     * 
+     * @return {object} Phrase object chosen to be used
+     *
      */
     getRandomPhrase(){
         const phraseArray = this.phrases;
@@ -26,21 +24,43 @@ class Game {
         const randomNumber = Math.floor(Math.random() * phraseArrayLength);
         return phraseArray[randomNumber];
     };
-
     startGame(){
         const overlay = document.getElementById('overlay');
         const start = document.getElementById('btn__reset');
-
         this.activePhrase = this.getRandomPhrase();
         start.addEventListener('click', (e) => {
             overlay.style.display = 'none';
-            }        
-        );
+            this.getRandomPhrase()
+            const button = document.getElementsByClassName('key');
+                for (let i=0; i<button.length;i++){
+                    button[i].classList.remove('chosen');
+                    button[i].classList.remove('wrong');
+                }
+            const lis = document.querySelectorAll('.letter');
+            lis.forEach(li => li.remove());
+            phrase.addPhraseToDisplay();
+            });
+        }
+    /**
+    * Handles onscreen keyboard button clicks
+    * @param (HTMLButtonElement) button - The clicked button element
+    */
+    handleInteraction(button) {
+        button = document.getElementsByClassName('key');
+        for (let i=0; i<button.length;i++){
+            button[i].addEventListener('click', (e) => {
+                let select = e.target.innerHTML;
+                if (phrase.checkLetter(select)){
+                    phrase.showMatchedLetter(select);
+                    button[i].classList.add('chosen');
+                } else {
+                    button[i].classList.add('wrong');
+                    this.removeLife();
+                }
+                this.gameOver(this.checkForWin());
+            }
+        )}
     };
-
-    handleInteraction(){
-    };
-
     /**
     * Checks for winning move
     * @return {boolean} True if game has been won, false if game wasn't
@@ -59,28 +79,45 @@ class Game {
         if (answer.every((value) => guesses.includes(value))){
             return true;
         } else {
-            return false; 
-        }   
+            return false;
+        }
     };
-
         /**
     * Increases the value of the missed property
     * Removes a life from the scoreboard
     * Checks if player has remaining lives and ends game if player is out
     */
     removeLife() {
-        let imgs = document.querySelectorAll('img');
-        console.log([...imgs]);
-        
-    }
-
+        const over = document.querySelector("#game-over-message");
+        const title = document.querySelector(".title");
+        const imgs = document.querySelectorAll('img');
+        const lives = [...imgs];
+        if(this.missed !== lives.length-1){
+            lives[this.missed].src = 'images/lostHeart.png';
+            this.missed++;
+        } else{
+            over.innerHTML = "Sorry, better luck next time!"
+            over.classList.add('title');
+            overlay.style.display = 'block';
+            overlay.classList.add('lose');
+            this.missed = 0;
+            if (this.missed === 0){
+                imgs.forEach(img => img.src = 'images/liveHeart.png');
+                }
+            }
+        };
         /**
     * Displays game over message
     * @param {boolean} gameWon - Whether or not the user won the game
     */
-    gameOver(gameWon) {};
-
+    gameOver(gameWon) {
+        const overlay = document.getElementById('overlay');
+        const over = document.querySelector("#game-over-message");
+        if(gameWon){
+            over.innerHTML = "Great Job!"
+            over.classList.add('title');
+            overlay.classList.add('win');
+            overlay.style.display = 'block';
+        }
+    };
 }
-
-
-
--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
 /* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
-
+* Project 4 - OOP Game App
+* app.js */
 const game = new Game();
 const phrase = new Phrase();
 game.startGame();
 phrase.addPhraseToDisplay();
-
 game.activePhrase = new Phrase(phrase.phrase);
-let letter = this;
 game.activePhrase.checkLetter(letter);
 game.activePhrase.showMatchedLetter(letter);
-game.removeLife();
+game.handleInteraction();

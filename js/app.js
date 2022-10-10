f21c4ce35345e3e84cca3bc1c364cf5885b2c058
Eric Agbayani <eric.agbayani@yahoo.com>
2022-10-09 21:56:18 -0700

    update

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const phrase = new Phrase();
 game.startGame();
 phrase.addPhraseToDisplay();
 game.activePhrase = new Phrase(phrase.phrase);
+let letter = this;
 game.activePhrase.checkLetter(letter);
 game.activePhrase.showMatchedLetter(letter);
 game.handleInteraction();

18d4e18e5073432c0c264f2d473c25dd0170451c
Eric Agbayani <52845467+yawwp@users.noreply.github.com>
2022-10-10 17:02:45 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 /* Treehouse FSJS Techdegree
-* Project 4 - OOP Game App
-* app.js */
+ * Project 4 - OOP Game App
+ * app.js */
+
 const game = new Game();
+game.phrases = [
+    {phrase:'say hi'},
+    {phrase:'hello there'},
+    {phrase:'puppy junior'},
+    {phrase:'fish tacos'}];
+
 const phrase = new Phrase();
+phrase.phrase = game.phrases;
+
 game.startGame();
 phrase.addPhraseToDisplay();
 game.activePhrase = new Phrase(phrase.phrase);
-let letter = this;
-game.activePhrase.checkLetter(letter);
-game.activePhrase.showMatchedLetter(letter);
+
+game.activePhrase.checkLetter(this);
+game.activePhrase.showMatchedLetter(this);
 game.handleInteraction();

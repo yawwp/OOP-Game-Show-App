1dfff5e17ad27386efe5ccfe71b9ff99491c2c9d
Eric Agbayani <52845467+yawwp@users.noreply.github.com>
2022-10-14 08:44:40 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,14 +2,29 @@
  * Project 4 - OOP Game App
  * app.js */
 
+const letters = document.querySelectorAll('.key');
+const start = document.getElementById('btn__reset');
+
 const game = new Game();
 const phrase = new Phrase();
+
+start.addEventListener('click', (e) => {
+    return game.startGame();
+});
+
+const buttons = [...letters];
+for (let i=0;i<buttons.length;i++){
+buttons[i].addEventListener('click', (e) => {
+    if(buttons[i].tagName === "BUTTON"){
+        let button = buttons[i];
+        return game.handleInteraction(button);
+    };
+})}   
+
+
 phrase.phrase = game.phrases;
-
-game.startGame();
 phrase.addPhraseToDisplay();
+game.removeLife();
 game.activePhrase = new Phrase(phrase.phrase);
-
 game.activePhrase.checkLetter(this);
 game.activePhrase.showMatchedLetter(this);
-game.handleInteraction();
